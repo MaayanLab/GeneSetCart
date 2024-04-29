5087b34a48d174708a2a34011b4c0dee26129dc7
stephanieolaiya <stephanieolaiya@gmail.com>
2024-04-29 17:26:01 -0400

    update next-config

--- imagineset/next.config.js
+++ imagineset/next.config.js
@@ -6,5 +6,26 @@ module.exports = {
         bodySizeLimit: '3mb',
       },
     },
+    async headers() {
+      return [
+        {
+          source: "/api/:path*",
+          headers: [
+            {
+              key: "Access-Control-Allow-Origin",
+              value: "*", // Set your origin
+            },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET, POST, PUT, DELETE, OPTIONS",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value: "Content-Type, Authorization",
+            },
+          ],
+        },
+      ];
+    },
   }
 // module.exports = nextConfig

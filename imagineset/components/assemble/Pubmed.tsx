496dcb43bd0b8c540fc84b8c1e7e1c14e49f08b4
stephanieolaiya <stephanieolaiya@gmail.com>
2024-05-20 16:09:32 -0400

    add autorif option to pubmed search

--- imagineset/components/assemble/Pubmed.tsx
+++ imagineset/components/assemble/Pubmed.tsx
@@ -1,7 +1,8 @@
 'use client'
 import {
   Button, Container,
-  Grid, Stack, TextField,
+  Grid, Stack, Switch, TextField,
+  Tooltip,
   Typography,
   useMediaQuery,
   useTheme
@@ -17,6 +18,8 @@ import { useParams } from "next/navigation";
 import { addStatus } from "./fileUpload/SingleUpload";
 import cache from "@/lib/cache";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -63,6 +66,7 @@ export default function GeneshotSearch() {
   const [validGenes, setValidGenes] = React.useState<string[]>([])
   const [loading, setLoading] = React.useState(false)
   const [status, setStatus] = React.useState<addStatus>({})
+  const [geneRIF, setGeneRIF] = React.useState(true)
   React.useEffect(() => {
     checkValidGenes(foundGenes.toString().replaceAll(',', '\n')).then((response) => setValidGenes(response))
   }, [foundGenes])
@@ -71,7 +75,8 @@ export default function GeneshotSearch() {
     query: string
   ) => {
     setLoading(true)
-    const cachedResult: any = cache.get(query);
+    const rif = geneRIF ? 'generif' : 'autorif'
+    const cachedResult: any = cache.get(query + '-' + rif);
     if (cachedResult === undefined) {
       fetch("https://maayanlab.cloud/geneshot/api/search",
         {
@@ -80,13 +85,13 @@ export default function GeneshotSearch() {
             'Content-Type': 'application/json'
           },
           method: "POST",
-          body: JSON.stringify({ "rif": "generif", "term": query })
+          body: JSON.stringify({ "rif": rif, "term": query })
         })
         .then((response) => response.json()
           .then((data) => {
             setLoading(false)
             const genes = Object.keys(data['gene_count'])
-            const success = cache.set(query, genes, 10000)
+            const success = cache.set(query + '-' + rif, genes, 10000)
             setFoundGenes(genes)
           }))
         .catch((res) => { setLoading(false); console.log(res) })
@@ -94,31 +99,54 @@ export default function GeneshotSearch() {
       setLoading(false)
       setFoundGenes(cachedResult ? cachedResult : [])
     }
-  }, [])
-
+  }, [geneRIF])
   return (
-    <Container maxWidth={false} sx={{width: '100%'}}>
+    <Container maxWidth={false} sx={{ width: '100%' }}>
       <Typography variant="h3" color="secondary.dark" className='p-5'>SEARCH GENE SETS FROM PUBMED</Typography>
       <Typography variant="subtitle1" color="#666666" sx={{ mb: 3, ml: 3 }}>
-        Enter a search term to obtain all genes mentioned with that term in publications according to GeneRIF
+        Enter a search term to obtain all genes mentioned with that term in publications according to GeneRIF or AutoRIF.
       </Typography>
-      <Stack direction='column' width={'100%'} sx={{ ml: 3 }}> 
-          {loading ? <LinearIndeterminate /> : <></>}
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-              name='search'
-              onKeyDown={(event) => {
-                if (event.key === 'Enter') {
-                  handleSearch((event.target as HTMLFormElement).value)
-                }
-              }}
-            />
-          </Search>
+      <Stack direction='column' width={'100%'} sx={{ ml: 3 }}>
+        {loading ? <LinearIndeterminate /> : <></>}
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+            name='search'
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                handleSearch((event.target as HTMLFormElement).value)
+              }
+            }}
+          />
+        </Search>
+        <Stack direction="row" spacing={1} justifyContent="center" alignItems="center">
+          <Tooltip title='AutoRIF automatically searches PubMed for the entered terms and returns lists of genes associated with the resulting publications. Genes are ranked by either the number of times that gene was associated with the terms (Count) or the Count divided by the number of times that gene is referenced in publications in general (Normalized Count).'>
+            <HelpOutlineIcon color="secondary"/>
+          </Tooltip>
+          <Typography color={'purple'}>AutoRIF</Typography>
+          <Switch
+            color="secondary"
+            checked={geneRIF}
+            onChange={() => setGeneRIF(!geneRIF)}
+            sx={{
+              "&.MuiSwitch-root .MuiSwitch-switchBase": {
+                color: "purple"
+              },
+            
+              "&.MuiSwitch-root .Mui-checked": {
+               color: "#336699"
+              }
+             }}
+          />
+          <Typography color={'secondary'} >GeneRIF</Typography>
+          <Tooltip title='Search PubMed using the manually-collected GeneRIF gene-term associations.'>
+            <HelpOutlineIcon color='secondary' />
+          </Tooltip>
+        </Stack>
       </Stack>
       <Grid container sx={{ p: 2 }} alignItems="center" justifyContent="center" display={isMobile ? 'block' : 'flex'} spacing={5} component={'form'}
         onSubmit={(evt) => {
@@ -134,19 +162,19 @@ export default function GeneshotSearch() {
               setStatus({ error: { selected: true, message: "Gene set already exists in this session!" } })
             } else {
               addToSessionSets(validGenes, sessionId, genesetName, description ? description : '')
-              .then((result) => { setStatus({ success: true }) })
-              .catch((err) => {
-                if (err.message === 'No valid genes in gene set') {
-                  setStatus({ error: { selected: true, message: err.message } })
-              } else {
-                  setStatus({ error: { selected: true, message: "Error in adding gene set!" } })
-              }
-              })
+                .then((result) => { setStatus({ success: true }) })
+                .catch((err) => {
+                  if (err.message === 'No valid genes in gene set') {
+                    setStatus({ error: { selected: true, message: err.message } })
+                  } else {
+                    setStatus({ error: { selected: true, message: "Error in adding gene set!" } })
+                  }
+                })
             }
           })
         }
         }>
-        <Grid direction='column' container item spacing={2} xs={isMobile ? 12 :5} justifyItems='center'>
+        <Grid direction='column' container item spacing={2} xs={isMobile ? 12 : 5} justifyItems='center'>
           <Grid item>
             <TextField id="outlined-basic" required label="Gene Set Name" variant="outlined" name='name' />
           </Grid>
@@ -175,7 +203,7 @@ export default function GeneshotSearch() {
           />
           <Grid item>
             <Button variant='contained' color="tertiary" type='submit'>
-            <AddShoppingCartIcon /> &nbsp; 
+              <AddShoppingCartIcon /> &nbsp;
               ADD TO CART
             </Button>
           </Grid>

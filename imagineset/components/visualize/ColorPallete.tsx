a248ac179faf79a9e625f66c598cac076e5e4f62
stephanieolaiya <stephanieolaiya@gmail.com>
2024-06-20 16:39:56 -0400

    Fix build error (add key to colormap divs)

--- imagineset/components/visualize/ColorPallete.tsx
+++ imagineset/components/visualize/ColorPallete.tsx
@@ -1,7 +1,7 @@
 export function ColorPallete({hexCodes}: {hexCodes: string[]}) {
     return (
         <div style={{ display: 'inline-flex', marginLeft: '3px' }}>
-            {hexCodes.map((color) => <div style={{ 'backgroundColor': color, width: '10px', height: '10px' }}></div>)}
+            {hexCodes.map((color, i) => <div  key={i} style={{ 'backgroundColor': color, width: '10px', height: '10px' }}></div>)}
         </div>
     )
 }

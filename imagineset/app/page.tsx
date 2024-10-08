ff9a5c9cb8d171238c41192fecc5facdbd767bfb
giacomomarino <77803629+giacomomarino@users.noreply.github.com>
2024-10-08 10:15:30 -0400

    fix client crashing on home page reload

--- imagineset/app/page.tsx
+++ imagineset/app/page.tsx
@@ -1,87 +1,12 @@
+"use server"
 import Header from '@/components/header/Header'
 import { StartButton } from '@/components/misc/HomePage/StartButton'
 import StyledCard from '@/components/misc/HomePage/StyledCards'
 import { Grid } from '@mui/material'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/lib/auth/authOptions'
-import prisma from '@/lib/prisma'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { PipelineSession, User } from '@prisma/client'
 import Cookies from 'js-cookie'
 
 export default async function Home({ params }: { params: { id: string } }) {
-  let sessionId = Cookies.get('session_id')
-  const session = await getServerSession(authOptions);
-  if (!sessionId) {
-    sessionId = cookies().get("session_id")?.value;
-  }
-  if (session && !sessionId) {
-    // if logged in but has no cookie, get most recently modified session
-    const sessionsRanked = await prisma.pipelineSession.findMany({
-      where: {
-        user_id: session.user.id,
-      },
-      orderBy: {
-        lastModified: "desc",
-      },
-    });
-    if (sessionsRanked.length > 0) {
-      const mostRecentSessionId = sessionsRanked[0].id;
-      sessionId = mostRecentSessionId;
-    }
-  }
-  if (sessionId) {
-    let user: (User & { pipelineSessions: PipelineSession[] }) | null;
-    if (session) {
-      user = await prisma.user.findFirst({
-        where: {
-          id: session.user.id,
-        },
-        include: {
-          pipelineSessions: true,
-        },
-      });
-    } else {
-      user = await prisma.user.findFirst({
-        where: {
-          id: process.env.PUBLIC_USER_ID,
-        },
-        include: {
-          pipelineSessions: true,
-        },
-      });
-    }
-    const userSessionArray = user?.pipelineSessions.map(
-      (sessionObject) => sessionObject.id
-    );
-    if (userSessionArray?.includes(sessionId)) {
-      const inOneDay = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-      Cookies.set("session_id", sessionId, {
-        secure: true,
-        expires: inOneDay,
-        sameSite: "None",
-      });
-      const sessionInfo = await prisma.pipelineSession.findUnique({
-        where: {
-          id: sessionId,
-        },
-        select: {
-          gene_sets: {
-            include: {
-              genes: true,
-            },
-            orderBy: {
-              createdAt: "desc",
-            },
-          },
-          id: true,
-          session_name: true,
-        },
-      });
-      sessionId = sessionInfo?.id
-    }
-  }
+  const sessionId = Cookies.get('session_id')
   return (
     <>
       <Grid item>
--- imagineset/components/misc/HomePage/StyledCards.tsx
+++ imagineset/components/misc/HomePage/StyledCards.tsx
@@ -1,171 +1,223 @@
-'use client'
+"use client";
 import React from "react";
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
-import assembleIcon from "@/public/img/otherLogos/assembleIcon.png"
-import visualizeIcon from "@/public/img/otherLogos/visualizeIcon.png"
-import augmentIcon from "@/public/img/otherLogos/augmentIcon.png"
-import analyzeIcon from "@/public/img/otherLogos/analyzeIcon.png"
-import combineIcon from "@/public/img/otherLogos/combineIcon.png"
+import assembleIcon from "@/public/img/otherLogos/assembleIcon.png";
+import visualizeIcon from "@/public/img/otherLogos/visualizeIcon.png";
+import augmentIcon from "@/public/img/otherLogos/augmentIcon.png";
+import analyzeIcon from "@/public/img/otherLogos/analyzeIcon.png";
+import combineIcon from "@/public/img/otherLogos/combineIcon.png";
 import { beginNewSession } from "./HomePageFunc";
-import Cookie from 'js-cookie'
 import Link from "next/link";
 import Image from "next/image";
-export default async function StyledCard( { sessionId }: { sessionId: string | undefined } ) {
+import Cookies from "js-cookie";
 
-    const Item = styled(Paper)(() => ({
-        // backgroundColor: '#cee1f5',
-        padding: 8,
-        textAlign: 'center',
-        // color: 'black',
-        minHeight: '12vw',
-        minWidth: '3vw',
-        border: 'solid 1px',
-        borderRadius: 20, 
-        borderColor: '#a7c4e4',
-        boxShadow: '12px 12px 10px 1px rgb(167 196 228)'
-    }));
+export default function StyledCard({
+  sessionId,
+}: {
+  sessionId: string | undefined;
+}) {
+  if (!sessionId) sessionId = Cookies.get("session_id");
 
-    const startSession = (page: string) => {
-        beginNewSession(page)
-    }
-    return (
-            <Grid container spacing={4}>
-                <Grid item xs={6} md={4}>
-                    {sessionId ?
-                    <Link href={`/assemble/${sessionId}`}>
-                    <Item elevation={3}>
-                        <Typography sx={{ typography: { sm: 'h4', xs: 'body2', fontWeight: 600 } }} color='secondary.dark' >
-                            ASSEMBLE
-                        </Typography>
-                        <div className="flex justify-center align-center mt-5">
-                            <div className="flex w-5/12">
-                                <Image src={assembleIcon} alt="" layout="fit"></Image>
-                            </div>
-                        </div>
-                    </Item>
-                    </Link> : 
-                    <div onClick={() => startSession('assemble')}>
-                    <Item elevation={3}>
-                        <Typography sx={{ typography: { sm: 'h4', xs: 'body2', fontWeight: 600 } }} color='secondary.dark' >
-                            ASSEMBLE
-                        </Typography>
-                        <div className="flex justify-center align-center mt-5">
-                            <div className="flex w-5/12">
-                                <Image src={assembleIcon} alt="" layout="fit"></Image>
-                            </div>
-                        </div>
-                    </Item>
-                    </div>} 
-                </Grid>
+  const Item = styled(Paper)(() => ({
+    // backgroundColor: '#cee1f5',
+    padding: 8,
+    textAlign: "center",
+    // color: 'black',
+    minHeight: "12vw",
+    minWidth: "3vw",
+    border: "solid 1px",
+    borderRadius: 20,
+    borderColor: "#a7c4e4",
+    boxShadow: "12px 12px 10px 1px rgb(167 196 228)",
+  }));
 
-                <Grid item xs={6} md={4}>
-                {sessionId ? <Link href={`/augment/${sessionId}`}>
-                    <Item elevation={3}>
-                        <Typography sx={{ typography: { sm: 'h4', xs: 'body2', fontWeight: 600 } }} color='secondary.dark'>
-                            AUGMENT
-                        </Typography>
-                        <div className="flex justify-center align-center mt-5">
-                            <div className="flex w-5/12">
-                                <Image src={augmentIcon} alt="" layout="fit"></Image>
-                            </div>
-                        </div>
-                    </Item>
-                    </Link>:
-                    <div onClick={() => startSession('augment')}>
-                    <Item elevation={3}>
-                        <Typography sx={{ typography: { sm: 'h4', xs: 'body2', fontWeight: 600 } }} color='secondary.dark'>
-                            AUGMENT
-                        </Typography>
-                        <div className="flex justify-center align-center mt-5">
-                            <div className="flex w-5/12">
-                                <Image src={augmentIcon} alt="" layout="fit"></Image>
-                            </div>
-                        </div>
-                    </Item>
-                    </div>}
-                </Grid>
+  const startSession = (page: string) => {
+    beginNewSession(page);
+  };
+  return (
+    <Grid container spacing={4}>
+      <Grid item xs={6} md={4}>
+        {sessionId != undefined ? (
+          <div onClick={() => window.location.replace(`/assemble/${sessionId}`)} className="cursor-pointer">
+            <Item elevation={3}>
+              <Typography
+                sx={{ typography: { sm: "h4", xs: "body2", fontWeight: 600 } }}
+                color="secondary.dark"
+              >
+                ASSEMBLE
+              </Typography>
+              <div className="flex justify-center align-center mt-5">
+                <div className="flex w-5/12">
+                  <Image src={assembleIcon} alt="" layout="fit"></Image>
+                </div>
+              </div>
+            </Item>
+          </div>
+        ) : (
+          <div onClick={() => startSession("assemble")} className="cursor-pointer">
+            <Item elevation={3}>
+              <Typography
+                sx={{ typography: { sm: "h4", xs: "body2", fontWeight: 600 } }}
+                color="secondary.dark"
+              >
+                ASSEMBLE
+              </Typography>
+              <div className="flex justify-center align-center mt-5">
+                <div className="flex w-5/12">
+                  <Image src={assembleIcon} alt="" layout="fit"></Image>
+                </div>
+              </div>
+            </Item>
+          </div>
+        )}
+      </Grid>
 
-                <Grid item xs={6} md={4}>
-                    {sessionId ? <Link href={`/combine/${sessionId}`}>
-                    <Item elevation={3}>
-                        <Typography sx={{ typography: { sm: 'h4', xs: 'body2', fontWeight: 600 } }} color='secondary.dark'>
-                            COMBINE
-                        </Typography>
-                        <div className="flex justify-center align-center mt-5">
-                            <div className="flex w-5/12">
-                                <Image src={combineIcon} alt="" layout="fit"></Image>
-                            </div>
-                        </div>
-                    </Item>
-                    </Link> : 
-                    <div onClick={() => startSession('combine')}>
-                    <Item elevation={3}>
-                        <Typography sx={{ typography: { sm: 'h4', xs: 'body2', fontWeight: 600 } }} color='secondary.dark'>
-                            COMBINE
-                        </Typography>
-                        <div className="flex justify-center align-center mt-5">
-                            <div className="flex w-5/12">
-                                <Image src={combineIcon} alt="" layout="fit"></Image>
-                            </div>
-                        </div>
-                    </Item>
-                    </div>}
-                </Grid>
+      <Grid item xs={6} md={4}>
+        {sessionId != undefined ? (
+          <div onClick={() => window.location.replace(`/augment/${sessionId}`)} className="cursor-pointer">
+            <Item elevation={3}>
+              <Typography
+                sx={{ typography: { sm: "h4", xs: "body2", fontWeight: 600 } }}
+                color="secondary.dark"
+              >
+                AUGMENT
+              </Typography>
+              <div className="flex justify-center align-center mt-5">
+                <div className="flex w-5/12">
+                  <Image src={augmentIcon} alt="" layout="fit"></Image>
+                </div>
+              </div>
+            </Item>
+          </div>
+        ) : (
+          <div onClick={() => startSession("augment")} className="cursor-pointer">
+            <Item elevation={3}>
+              <Typography
+                sx={{ typography: { sm: "h4", xs: "body2", fontWeight: 600 } }}
+                color="secondary.dark"
+              >
+                AUGMENT
+              </Typography>
+              <div className="flex justify-center align-center mt-5">
+                <div className="flex w-5/12">
+                  <Image src={augmentIcon} alt="" layout="fit"></Image>
+                </div>
+              </div>
+            </Item>
+          </div>
+        )}
+      </Grid>
 
-                <Grid item xs={6} md={6}>
-                    {sessionId ? <Link href={`/visualize/${sessionId}`}> 
-                    <Item elevation={3}>
-                        <Typography sx={{ typography: { sm: 'h4', xs: 'body2', fontWeight: 600 } }} color='secondary.dark'>
-                            VISUALIZE
-                        </Typography>
-                        <div className="flex justify-center align-center mt-5">
-                            <div className="flex w-5/12 md:w-3/12" >
-                                <Image src={visualizeIcon} alt="" layout="cover"></Image>
-                            </div>
-                        </div>
-                    </Item>
-                    </Link> : <div onClick={() => startSession('visualize')}>
-                    <Item elevation={3}>
-                        <Typography sx={{ typography: { sm: 'h4', xs: 'body2', fontWeight: 600 } }} color='secondary.dark'>
-                            VISUALIZE
-                        </Typography>
-                        <div className="flex justify-center align-center mt-5">
-                            <div className="flex w-5/12 md:w-3/12" >
-                                <Image src={visualizeIcon} alt="" layout="cover"></Image>
-                            </div>
-                        </div>
-                    </Item>
-                    </div>}
-                </Grid>
+      <Grid item xs={6} md={4}>
+        {sessionId != undefined ? (
+          <div onClick={() => window.location.replace(`/combine/${sessionId}`)} className="cursor-pointer">
+            <Item elevation={3}>
+              <Typography
+                sx={{ typography: { sm: "h4", xs: "body2", fontWeight: 600 } }}
+                color="secondary.dark"
+              >
+                COMBINE
+              </Typography>
+              <div className="flex justify-center align-center mt-5">
+                <div className="flex w-5/12">
+                  <Image src={combineIcon} alt="" layout="fit"></Image>
+                </div>
+              </div>
+            </Item>
+          </div>
+        ) : (
+          <div onClick={() => startSession("combine")} className="cursor-pointer">
+            <Item elevation={3}>
+              <Typography
+                sx={{ typography: { sm: "h4", xs: "body2", fontWeight: 600 } }}
+                color="secondary.dark"
+              >
+                COMBINE
+              </Typography>
+              <div className="flex justify-center align-center mt-5">
+                <div className="flex w-5/12">
+                  <Image src={combineIcon} alt="" layout="fit"></Image>
+                </div>
+              </div>
+            </Item>
+          </div>
+        )}
+      </Grid>
 
-                <Grid item xs={12} md={6}>
-                    {sessionId ? <Link href={`/analyze/${sessionId}`}> 
-                    <Item elevation={3}>
-                        <Typography sx={{ typography: { sm: 'h4', xs: 'body2', fontWeight: 600 } }} color='secondary.dark'>
-                            ANALYZE
-                        </Typography>
-                        <div className="flex justify-center align-center mt-5">
-                            <div className="flex w-5/12 md:w-3/12">
-                                <Image src={analyzeIcon} alt="" layout="fit"></Image>
-                            </div>
-                        </div>
-                    </Item>
-                    </Link> : <div onClick={() => startSession('analyze')}>
-                    <Item elevation={3}>
-                        <Typography sx={{ typography: { sm: 'h4', xs: 'body2', fontWeight: 600 } }} color='secondary.dark'>
-                            ANALYZE
-                        </Typography>
-                        <div className="flex justify-center align-center mt-5">
-                            <div className="flex w-5/12 md:w-3/12">
-                                <Image src={analyzeIcon} alt="" layout="fit"></Image>
-                            </div>
-                        </div>
-                    </Item>
-                    </div>}
-                </Grid>
-            </Grid>
-    )
+      <Grid item xs={6} md={6}>
+        {sessionId != undefined ? (
+          <div onClick={() => window.location.replace(`/visualize/${sessionId}`)} className="cursor-pointer">
+            <Item elevation={3}>
+              <Typography
+                sx={{ typography: { sm: "h4", xs: "body2", fontWeight: 600 } }}
+                color="secondary.dark"
+              >
+                VISUALIZE
+              </Typography>
+              <div className="flex justify-center align-center mt-5">
+                <div className="flex w-5/12 md:w-3/12">
+                  <Image src={visualizeIcon} alt="" layout="cover"></Image>
+                </div>
+              </div>
+            </Item>
+          </div>
+        ) : (
+          <div onClick={() => startSession("visualize")} className="cursor-pointer">
+            <Item elevation={3}>
+              <Typography
+                sx={{ typography: { sm: "h4", xs: "body2", fontWeight: 600 } }}
+                color="secondary.dark"
+              >
+                VISUALIZE
+              </Typography>
+              <div className="flex justify-center align-center mt-5">
+                <div className="flex w-5/12 md:w-3/12">
+                  <Image src={visualizeIcon} alt="" layout="cover"></Image>
+                </div>
+              </div>
+            </Item>
+          </div>
+        )}
+      </Grid>
+
+      <Grid item xs={12} md={6}>
+        {sessionId != undefined ? (
+          <div onClick={() => window.location.replace(`/analyze/${sessionId}`)} className="cursor-pointer">
+            <Item elevation={3}>
+              <Typography
+                sx={{ typography: { sm: "h4", xs: "body2", fontWeight: 600 } }}
+                color="secondary.dark"
+              >
+                ANALYZE
+              </Typography>
+              <div className="flex justify-center align-center mt-5">
+                <div className="flex w-5/12 md:w-3/12">
+                  <Image src={analyzeIcon} alt="" layout="fit"></Image>
+                </div>
+              </div>
+            </Item>
+          </div>
+        ) : (
+          <div onClick={() => startSession("analyze")} className="cursor-pointer">
+            <Item elevation={3}>
+              <Typography
+                sx={{ typography: { sm: "h4", xs: "body2", fontWeight: 600 } }}
+                color="secondary.dark"
+              >
+                ANALYZE
+              </Typography>
+              <div className="flex justify-center align-center mt-5">
+                <div className="flex w-5/12 md:w-3/12">
+                  <Image src={analyzeIcon} alt="" layout="fit"></Image>
+                </div>
+              </div>
+            </Item>
+          </div>
+        )}
+      </Grid>
+    </Grid>
+  );
 }
